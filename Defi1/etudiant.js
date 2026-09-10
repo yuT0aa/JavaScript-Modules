a014392501648700b2459bb8290fd0f829d3585f
SMILE <achraf.arajoum@gmail.com>
2026-09-10 12:15:31 +0100

    Create etudiant.js

---
+++ Defi1/etudiant.js
@@ -0,0 +1,19 @@
+let students=[{
+    id: 1,
+    name: "Ahmed",
+    age: 20,
+    notes: [12, 15, 17]
+},{
+    id: 2,
+    name: "Sam",
+    age: 18,
+    notes: [20, 16, 19]
+},{
+    id: 3,
+    name: "Omar",
+    age: 22,
+    notes: [6, 11, 10]
+}];
+
+
+
